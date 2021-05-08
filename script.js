6858ff195299ef7a68fb70d54849220e038c1d20
Estasie <anastasiaberia00@gmail.com>
2021-05-08 16:15:34 +0300

    form added
    Also added:
    - nav anchors;
    - form styles;
    - social icons.
    To be fixed:
    - form styles

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log(typeof(undefined));
+document.addEventListener("DOMContentLoaded", () => {
+  const openFormButton = document.querySelectorAll('.bookButton'),
+        pricesFormButton = document.querySelectorAll('.book'),
+        anchors = document.querySelectorAll('a[href*="#"]'),
+        closeButton = document.querySelector('.closeButton'),
+        callForm = document.querySelector('.formContainer');
+
+  for (let anchor of anchors) {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const blockID = anchor.getAttribute("href").substr(1);
+      console.log(blockID);
+      document.getElementById(blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
+
+  openFormButton.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        callForm.classList.remove('hidden');
+    });
+  });
+
+  pricesFormButton.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        callForm.classList.remove('hidden');
+    });
+  });
+
+  closeButton.addEventListener('click', e => {
+    callForm.classList.add('hidden');
+  });
+});
